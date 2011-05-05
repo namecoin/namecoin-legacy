8aceb3b1ecf211cf85cbd98970a8353c1edd7291
Vincent Durham <vinced@vinced.bit>
2011-05-05 03:28:51 -0400

    pass mapTestPool to ConnectInputs hook

--- src/hook.cpp
+++ src/hook.cpp
@@ -13,6 +13,7 @@ public:
     virtual void AddToWallet(CWalletTx& tx);
     virtual bool CheckTransaction(const CTransaction& tx);
     virtual bool ConnectInputs(CTxDB& txdb,
+            map<uint256, CTxIndex>& mapTestPool,
             const CTransaction& tx,
             vector<CTransaction>& vTxPrev,
             vector<CTxIndex>& vTxindex,
@@ -87,6 +88,7 @@ bool CStandardHooks::CheckTransaction(const CTransaction& tx)
 }
 
 bool CStandardHooks::ConnectInputs(CTxDB& txdb,
+        map<uint256, CTxIndex>& mapTestPool,
         const CTransaction& tx,
         vector<CTransaction>& vTxPrev,
         vector<CTxIndex>& vTxindex,
