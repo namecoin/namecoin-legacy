95ca4f8dc800a203f65613afe5c9c79894d6c6a7
nelisky <nelisky.btc@gmail.com>
2013-05-30 21:08:12 +0100

    Using proper namecoin address versions

--- src/base58.h
+++ src/base58.h
@@ -249,7 +249,7 @@ public:
     void SetSecret(const CSecret32& vchSecret, bool fCompressed)
     {
         assert(vchSecret.size() == 32);
-        SetData(fTestNet ? 239 : 128, &vchSecret[0], vchSecret.size());
+        SetData(fTestNet ? 111 : 52, &vchSecret[0], vchSecret.size());
         if (fCompressed)
             vchData.push_back(1);
     }
@@ -268,10 +268,10 @@ public:
         bool fExpectTestNet = false;
         switch(nVersion)
         {
-            case 128:
+            case 52:
                 break;
 
-            case 239:
+            case 111:
                 fExpectTestNet = true;
                 break;
 
