e29c848d695920fb3aaa3811ebb5952e030c23e2
phelixnmc <githubnmc@blockchained.com>
2013-09-02 12:04:13 +0200

    EasyWinBuilder v0.3

---
+++ contrib/easywinbuilder/hash_qt.sh
@@ -0,0 +1,6 @@
+echo Hash of Qt binary [experimental]...
+bash dahash.sh $ROOTPATHSH/release/${COINNAME}-qt.exe
+echo phelix got: 6ebd7bb9ff0bb85a2328527e99357ada89cd2ed57726395188ea7b5ea453284b
+echo
+echo
+
