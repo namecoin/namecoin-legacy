c71cc7814859e519e836b58ad74e048e561da45d
phelixnmc <githubnmc@blockchained.com>
2014-06-30 21:22:53 +0200

    Contrib EasyWinBuilder: Fix Qt wait position with included MinGW

--- contrib/easywinbuilder/1b_environment_qt.bat
+++ contrib/easywinbuilder/1b_environment_qt.bat
@@ -1,7 +1,7 @@
 @call set_vars.bat
 @echo About to download Qt installer - you need to install it manually:
 @echo  Use default directory "%QTBASEPATH%".
-@echo  Activate checkbox: tools - MinGW
+@echo  Activate checkbox: tools - MinGW !!!
 @echo.
 @pause
 @start %QTDOWNLOADPATH%
